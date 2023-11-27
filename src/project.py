e9222ff6f4d656dd377e57cd69c7ecd7e79ce691
De'Ja Kelly <dmk220005@utdallas.edu>
2023-11-26 19:10:18 -0600

    moved game loop code into defined main function

--- src/project.py
+++ src/project.py
@@ -1,24 +1,25 @@
 import pygame
 import sys
 
-pygame.init()
 
-screen_width = 750
-screen_height = 700
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Rockett Kitty Blast")
+def main():
+    pygame.init()
+    screen_width = 750
+    screen_height = 700
+    gray = (29, 29, 27)
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Rockett Kitty Blast")
+    #clock = pygame.time.Clock()
+    while True:
+        # Checking for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        screen.fill(gray)
+        #pygame.display.update()
+        #clock.tick(60)
 
-#clock = pygame.time.Clock()
-
-while True:
-    # Checking for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    # Drawing
-    
-    #pygame.display.update()
-    #clock.tick(60)
+if __name__ == "__main__":
+    main()
