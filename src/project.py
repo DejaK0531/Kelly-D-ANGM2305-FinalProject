723b5f9c2ab14e62b5fccd203e66aa00a53be099
De'Ja Kelly <dmk220005@utdallas.edu>
2023-11-27 15:01:14 -0600

    fixed bug with player icon - working

--- src/project.py
+++ src/project.py
@@ -1,12 +1,6 @@
 import pygame
 import sys
 
-player_img = pygame.image.load('rocket_kitty_player.png')
-
-def player():
-    screen = pygame.display.set_mode()
-    screen.blit(player_img, (370, 480))
-    player()
 
 def main():
     pygame.init()
@@ -15,12 +9,12 @@ def main():
     screen = pygame.display.set_mode((screen_width, screen_height))
     intro_bg = pygame.image.load('intro_bg.png')
     background = pygame.image.load('rocket_kitty_bg.png')
+    player_img = pygame.image.load('rocket_kitty_player.png')
     icon = pygame.image.load('rocket_kitty_icon.png')
     pygame.display.set_icon(icon)
     pygame.display.set_caption("Rocket Kitty Blast!")
     clock = pygame.time.Clock()
 
-    
     # fade in the logo
     fade_duration = 2000  # in milliseconds (2 seconds)
     fade_start_time = pygame.time.get_ticks()
@@ -40,6 +34,11 @@ def main():
         else:
             # Image stays on screen for a few seconds before fading out
             screen.blit(pygame.transform.scale(background, (750, 660)), (0, 0))
+            
+             # Display rocket sprite at the bottom center of the screen
+            rocket_position = (screen_width // 2 - player_img.get_width() // 2,
+                               screen_height - player_img.get_height())
+            screen.blit(player_img, rocket_position)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
