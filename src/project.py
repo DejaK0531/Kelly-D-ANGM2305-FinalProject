957a2e6aee4d3f2763ae8b87db9d5fd764fec472
De'Ja Kelly <dmk220005@utdallas.edu>
2023-11-27 00:01:02 -0600

    fixed bug with background appearing

--- src/project.py
+++ src/project.py
@@ -12,19 +12,18 @@ def main():
     icon = pygame.image.load('rocket_kitty.png')
     pygame.display.set_icon(icon)
     clock = pygame.time.Clock()
-    background = (0, 0, 0)
+    #background = (0, 0, 0)
     #intro_bg = pygame.image.load('bg.png')
     background = pygame.image.load('rocket_kitty_bg.png')
 
     running = True
     while running:
         # Checking for events
-        screen.blit(background, (0, 0))
+        screen.blit(background,(0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        screen.fill(background)
         pygame.display.update()
         clock.tick(60)
 
