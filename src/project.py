adb4ea75e93fd316a57d0e49fab63434154d8e92
De'Ja Kelly <dmk220005@utdallas.edu>
2023-11-26 23:46:44 -0600

    added icon sprite to the top of the window bar

--- src/project.py
+++ src/project.py
@@ -7,15 +7,19 @@ def main():
     pygame.init()
     screen_width = 750
     screen_height = 660
-    background = (0, 0, 0)
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Rocket Kitty Blast")
     icon = pygame.image.load('rocket_kitty.png')
     pygame.display.set_icon(icon)
     clock = pygame.time.Clock()
+    background = (0, 0, 0)
+    #intro_bg = pygame.image.load('bg.png')
+    background = pygame.image.load('bg.png')
+
     running = True
     while running:
         # Checking for events
+        screen.blit(background, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
