2b1fc5d76d3920e710e78db7234cedb15c651226
De'Ja Kelly <dmk220005@utdallas.edu>
2023-11-27 16:47:49 -0600

    combined initialized and main game loop functions

--- src/project.py
+++ src/project.py
@@ -1,72 +1,6 @@
 import pygame
 import sys
 
-def main():
-    screen, intro_bg, background, player_img, clock, fade_duration, fade_start_time = initialize()
-
-    # fade in the logo
-    fade_in_intro(screen, intro_bg, fade_duration, fade_start_time)
-
-    # Added player_x and player_y for rocket position
-    player_x = screen.get_width() // 2 - player_img.get_width() // 2
-    player_y = screen.get_height() - player_img.get_height()
-
-    # Added player_speed for rocket movement speed
-    player_speed = 5  # Adjust the speed as needed
-
-    running = True
-    while running:
-        # Checking for events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and player_x > 0:
-            player_x -= player_speed
-        if keys[pygame.K_RIGHT] and player_x < screen.get_width() - player_img.get_width():
-            player_x += player_speed
-
-        # Continue with the main game loop
-        current_time = pygame.time.get_ticks()
-        elapsed_time = current_time - fade_start_time
-
-        if elapsed_time < fade_duration:
-            alpha = int((elapsed_time / fade_duration) * 255)
-            intro_bg.set_alpha(alpha)
-            screen.blit(pygame.transform.scale(intro_bg, (750, 660)), (0, 0))
-
-        else:
-            # Image stays on screen for a few seconds before fading out
-            screen.blit(pygame.transform.scale(background, (750, 660)), (0, 0))
-            
-            # Display rocket sprite at the updated position
-            screen.blit(player_img, (player_x, player_y))
-
-        pygame.display.flip()
-        clock.tick(30)
-    
-    pygame.quit()
-    sys.exit()
-    pygame.display.update()
-
-def initialize():
-    pygame.init()
-    screen_width = 750
-    screen_height = 660
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    intro_bg = pygame.image.load('intro_bg.png')
-    background = pygame.image.load('rocket_kitty_bg.png')
-    player_img = pygame.image.load('rocket_kitty_player.png')
-    icon = pygame.image.load('rocket_kitty_icon.png')
-    pygame.display.set_icon(icon)
-    pygame.display.set_caption("Rocket Kitty Blast!")
-    clock = pygame.time.Clock()
-    fade_duration = 2000  # in milliseconds (2 seconds)
-    fade_start_time = pygame.time.get_ticks()
-
-    return screen, intro_bg, background, player_img, clock, fade_duration, fade_start_time
-
 def fade_in_intro(screen, intro_bg, fade_duration, fade_start_time):
     pygame.time.delay(15)  # Add a small delay for a smoother fade-in
 
@@ -83,5 +17,67 @@ def fade_in_intro(screen, intro_bg, fade_duration, fade_start_time):
         screen.blit(pygame.transform.scale(intro_bg, (750, 660)), (0, 0))
         pygame.display.flip()
 
+def initialize_and_run_game():
+    pygame.init()
+    screen_width = 750
+    screen_height = 660
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    intro_bg = pygame.image.load('intro_bg.png')
+    background = pygame.image.load('rocket_kitty_bg.png')
+    player_img = pygame.image.load('rocket_kitty_player.png')
+    icon = pygame.image.load('rocket_kitty_icon.png')
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption("Rocket Kitty Blast!")
+    clock = pygame.time.Clock()
+    fade_duration = 2000  # in milliseconds (2 seconds)
+    fade_start_time = pygame.time.get_ticks()
+
+    # fade in the logo
+    fade_in_intro(screen, intro_bg, fade_duration, fade_start_time)
+
+    player_x = screen.get_width() // 2 - player_img.get_width() // 2
+    player_y = screen.get_height() - player_img.get_height()
+
+    player_speed = 5  # Adjust the speed as needed
+
+    lasers = pygame.sprite.Group()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                laser = Laser(player_x + player_img.get_width() // 2, player_y)
+                lasers.add(laser)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player_x > 0:
+            player_x -= player_speed
+        if keys[pygame.K_RIGHT] and player_x < screen.get_width() - player_img.get_width():
+            player_x += player_speed
+
+        current_time = pygame.time.get_ticks()
+        elapsed_time = current_time - fade_start_time
+
+        if elapsed_time < fade_duration:
+            alpha = int((elapsed_time / fade_duration) * 255)
+            intro_bg.set_alpha(alpha)
+            screen.blit(pygame.transform.scale(intro_bg, (750, 660)), (0, 0))
+
+        else:
+            screen.blit(pygame.transform.scale(background, (750, 660)), (0, 0))
+            screen.blit(player_img, (player_x, player_y))
+
+        lasers.update()
+        lasers.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(30)
+    
+    pygame.quit()
+    sys.exit()
+    pygame.display.update()
+
 if __name__ == "__main__":
-    main()
+    initialize_and_run_game()
