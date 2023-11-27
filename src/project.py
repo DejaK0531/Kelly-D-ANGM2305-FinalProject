ef562786139a92186edf87b6a700dadd40e6a8a8
De'Ja Kelly <dmk220005@utdallas.edu>
2023-11-27 00:37:59 -0600

    finished importing intro background sprite

--- src/project.py
+++ src/project.py
@@ -8,13 +8,20 @@ def main():
     screen_width = 750
     screen_height = 660
     screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption("Rocket Kitty Blast")
+    intro_bg = pygame.image.load('intro_bg.png')
+    background = pygame.image.load('rocket_kitty_bg.png')
     icon = pygame.image.load('rocket_kitty.png')
     pygame.display.set_icon(icon)
+    pygame.display.set_caption("Rocket Kitty Blast")
     clock = pygame.time.Clock()
-    #background = (0, 0, 0)
-    #intro_bg = pygame.image.load('bg.png')
-    background = pygame.image.load('rocket_kitty_bg.png')
+
+    
+    # fade in the logo
+    for i in range(255):
+        screen.fill((0,0,0))
+        intro_bg.set_alpha(i)
+        screen.blit(pygame.transform.scale(intro_bg, (750, 660)), (0,0))
+        pygame.display.flip()
 
     running = True
     while running:
@@ -25,7 +32,7 @@ def main():
                 pygame.quit()
                 sys.exit()
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(20)
 
 if __name__ == "__main__":
     main()
