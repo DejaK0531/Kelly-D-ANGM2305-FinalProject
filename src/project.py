e86323e9ad2bc3513be96b0cf8472806eb9b0301
De'Ja Kelly <dmk220005@utdallas.edu>
2023-11-28 01:32:07 -0600

    made a second directory for drawing obstacle pattern

--- src/project.py
+++ src/project.py
@@ -1,27 +1,10 @@
 import pygame
 import sys
 
-class ObstacleManager:
-    def __init__(self, screen, grid):
-        self.screen = screen
-        self.grid = grid
-        self.obstacle_color = (255, 255, 255)  # Adjust the color as needed
-        self.cell_size = 3  # Adjust the cell size as needed
-
-    def draw_obstacles(self):
-        for row_index, row in enumerate(self.grid):
-            for col_index, cell in enumerate(row):
-                if cell == 1:
-                    pygame.draw.rect(
-                        self.screen,
-                        self.obstacle_color,
-                        (col_index * self.cell_size, row_index * self.cell_size, self.cell_size, self.cell_size)
-                    )
-
 class Laser(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((5, 20))
+        self.image = pygame.Surface((5, 20))  # Adjust the size as needed
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
@@ -30,7 +13,7 @@ class Laser(pygame.sprite.Sprite):
     def update(self):
         self.rect.y += self.speed
         if self.rect.bottom < 0:
-            self.kill()    
+            self.kill()
 
 def fade_in_intro(screen, intro_bg, fade_duration, fade_start_time):
     pygame.time.delay(15)  # Add a small delay for a smoother fade-in
@@ -63,31 +46,13 @@ def main():
     fade_duration = 2000  # in milliseconds (2 seconds)
     fade_start_time = pygame.time.get_ticks()
 
-
-    # Fade in the logo
+    # fade in the logo
     fade_in_intro(screen, intro_bg, fade_duration, fade_start_time)
 
-    # Player movement
     player_x = screen.get_width() // 2 - player_img.get_width() // 2
     player_y = screen.get_height() - player_img.get_height()
     player_speed = 5
 
-    grid = [
-    [0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0],
-    [0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0],
-    [0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
-    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
-    [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
-    [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1]
-    ]
-    obstacle_manager = ObstacleManager(screen, grid)
     lasers = pygame.sprite.Group()
 
     running = True
@@ -98,7 +63,7 @@ def main():
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 laser = Laser(player_x + player_img.get_width() // 2, player_y)
                 lasers.add(laser)
-        
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and player_x > 0:
             player_x -= player_speed
@@ -116,16 +81,15 @@ def main():
         else:
             screen.blit(pygame.transform.scale(background, (750, 660)), (0, 0))
             screen.blit(player_img, (player_x, player_y))
-            obstacle_manager.draw_obstacles()
 
         lasers.update()
         lasers.draw(screen)
+        
+
         pygame.display.flip()
         clock.tick(30)
-    
+
     pygame.quit()
     sys.exit()
-    pygame.display.update()
-
 if __name__ == "__main__":
     main()
