36ab7f874202053daee3b11adae6b997b669c4df
De'Ja Kelly <dmk220005@utdallas.edu>
2023-12-01 11:36:55 -0600

    edited tex and fixed collision bug

--- src/project.py
+++ src/project.py
@@ -15,7 +15,7 @@ class Player(pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
-        self.speed = 7
+        self.speed = 5
         self.visible = True
         self.alien_group = alien_group
         self.screen = screen
@@ -39,7 +39,7 @@ class Player(pygame.sprite.Sprite):
         # Only update if the player is visible
         if self.visible:
             self.screen.blit(self.image, self.rect.topleft)
-                
+
 class AlienLaser(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
@@ -47,7 +47,7 @@ class AlienLaser(pygame.sprite.Sprite):
         self.image.fill((247, 49, 15))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        self.speed = 5
+        self.speed = 6
 
     def update(self):
         self.rect.y += self.speed
@@ -65,7 +65,7 @@ class Alien(pygame.sprite.Sprite):
         self.active = True  # Flag to indicate whether the alien is active
 
     def shoot_laser(self, laser_group):
-        if random.randint(0, 500) < 3:  # Adjust the probability as needed
+        if random.randint(0, 200) < 2:  # Adjust the probability as needed
             laser = AlienLaser(self.rect.x + self.rect.width // 2, self.rect.y + self.rect.height)
             laser_group.add(laser)
 
@@ -102,7 +102,7 @@ class Laser(pygame.sprite.Sprite):
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        self.speed = -60
+        self.speed = -40
         self.obstacle_group = obstacle_group
         self.alien_group = alien_group
 
@@ -131,7 +131,7 @@ class Obstacle(pygame.sprite.Sprite):
         "xxx     xxx",
         "xx       xx"
     ]
-    
+
     def __init__(self, screen_width):
         super().__init__()
         self.shape = Obstacle.shape
@@ -139,7 +139,7 @@ class Obstacle(pygame.sprite.Sprite):
         self.blocks = pygame.sprite.Group()
         self.obstacle_amount = 4
         self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
-        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width / 15, y_start = 505)
+        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start=screen_width / 15, y_start=505)
 
     def create_obstacle(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
@@ -151,6 +151,7 @@ class Obstacle(pygame.sprite.Sprite):
                     self.blocks.add(block)
 
     def create_multiple_obstacles(self, *offset, x_start, y_start):
+        self.blocks.empty()  # Clear existing obstacles before creating new ones
         for offset_x in offset:
             self.create_obstacle(x_start, y_start, offset_x)
 
@@ -170,7 +171,7 @@ def fade_in_intro(screen, intro_bg, fade_duration, fade_start_time):
         pygame.display.flip()
 
 def main():
-    global screen_width, screen_height, alien_group
+    global screen_width, screen_height, alien_group  # Declare variables as global
 
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
@@ -187,55 +188,103 @@ def main():
     # fade in the logo
     fade_in_intro(screen, intro_bg, fade_duration, fade_start_time)
 
-    alien_group = AlienGroup(number_of_aliens=15, alien_size=55)
+    # Move the initialization of alien_group before creating the Player instance
+    alien_group = AlienGroup(number_of_aliens = 15, alien_size = 70)
     alien_lasers = pygame.sprite.Group()
-    player = Player(screen_width // 2 - player_img.get_width() // 2, screen_height - player_img.get_height(), player_img, alien_group, screen)
+    player = Player(screen_width // 2 - player_img.get_width() // 2, screen_height - player_img.get_height(),
+                    player_img, alien_group, screen)
     lasers = pygame.sprite.Group()
     obstacle = Obstacle(screen_width)
 
+    game_over_font = pygame.font.Font(None, 74)
+    play_again_font = pygame.font.Font(None, 36)
+    winner_font = pygame.font.Font(None, 74)
+    game_over_text = game_over_font.render("Game Over", True, (122, 242, 226))
+    play_again_text = play_again_font.render("Press Enter to Play Again", True, (255, 255, 255))
+    winner_text = winner_font.render("Winner!", True, (6, 201, 145))
+    game_over_rect = game_over_text.get_rect(center=(screen_width // 2, screen_height // 2 - 50))
+    play_again_rect = play_again_text.get_rect(center=(screen_width // 2, screen_height // 2 + 50))
+    winner_rect = winner_text.get_rect(center=(screen_width // 2, screen_height // 2 - 50))
+
     running = True
+    game_over = False
+    winner = False
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not game_over:
                 laser = Laser(player.rect.x + player_img.get_width() // 2, player.rect.y, obstacle.blocks, alien_group)
                 lasers.add(laser)
+
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN and (game_over or winner):
+                # Reset all game-related variables
+                game_over = False
+                winner = False
+                lasers.empty()
+                alien_lasers.empty()
+                obstacle.create_multiple_obstacles(*obstacle.obstacle_x_positions, x_start=screen_width / 15, y_start=505)
+                alien_group.empty()
+                alien_group.create_aliens(15, 55)
+                player.visible = True
+                player.rect.topleft = (screen_width // 2 - player_img.get_width() // 2, screen_height - player_img.get_height())
+
 
         player.update()
 
         current_time = pygame.time.get_ticks()
         elapsed_time = current_time - fade_start_time
 
-        if elapsed_time >= fade_duration:  # Check if fading duration has elapsed
+        if elapsed_time < fade_duration:
+            alpha = int((elapsed_time / fade_duration) * 255)
+            intro_bg.set_alpha(alpha)
+            screen.blit(pygame.transform.scale(intro_bg, (screen_width, screen_height)), (0, 0))
+        else:
             screen.blit(pygame.transform.scale(background, (screen_width, screen_height)), (0, 0))
             screen.blit(player.image, player.rect.topleft)
 
+        if not game_over and not winner:  # Only update if the game is still running
             lasers.update()
             lasers.draw(screen)
             obstacle.blocks.update()
-            obstacle.blocks.draw(screen)
+            obstacle.blocks.draw(screen)  # Make sure to draw the obstacles
             alien_group.update(alien_lasers)
             alien_group.draw(screen)
+            
+            # Update and draw enemy lasers
             alien_lasers.update()
             alien_lasers.draw(screen)
 
-            # Check for collisions between player lasers and obstacles
+            # Check for collisions between lasers and obstacles
             laser_obstacle_collision = pygame.sprite.groupcollide(lasers, obstacle.blocks, True, True)
-            # Check for collisions between player lasers and aliens
+            # Check for collisions between lasers and aliens
             laser_alien_collision = pygame.sprite.groupcollide(lasers, alien_group, True, True)
 
-            # Check for collisions between alien lasers and obstacles
+            # Check for collisions between enemy lasers and obstacles
             alien_laser_obstacle_collision = pygame.sprite.groupcollide(alien_lasers, obstacle.blocks, True, True)
-            # Check for collisions between alien lasers and the player
-            alien_laser_player_collision = pygame.sprite.spritecollide(player, alien_lasers, True)
-            if alien_laser_player_collision:
+
+                # Check for collisions between player and alien lasers
+            player_alien_laser_collision = pygame.sprite.spritecollide(player, alien_lasers, True)
+            if player_alien_laser_collision:
+                # Implement player death actions here (e.g., make the player invisible)
                 player.visible = False
                 player.rect.topleft = (-100, -100)  # Move the player off-screen
                 alien_group.stop_aliens()  # Stop the aliens when player is killed
+                game_over = True  # Set the game over flag
 
-            pygame.display.flip()
-            clock.tick(30)
+                # Check for player victory
+            if not alien_group:  # If the alien group is empty, the player wins
+                winner = True  # Set the winner flag
+
+        if game_over:
+            screen.blit(game_over_text, game_over_rect)
+            screen.blit(play_again_text, play_again_rect)
+        elif winner:
+            screen.blit(winner_text, winner_rect)
+            screen.blit(play_again_text, play_again_rect)  # Add this line to display "Press Enter to Play Again" on the winner screen
+
+        pygame.display.flip()
+        clock.tick(30)
 
     pygame.quit()
     sys.exit()
